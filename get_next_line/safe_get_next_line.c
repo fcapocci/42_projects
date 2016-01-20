8c61fe76ab01012e7b4ef8b9c8dc68014ba2f8a4
Florent CAPOCCI <fcapocci@e1r5p11.42.fr>
2016-01-20 18:25:33 +0100

    gnl bonus ok error ret

--- get_next_line/safe_get_next_line.c
+++ get_next_line/safe_get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: fcapocci <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/04 11:21:34 by fcapocci          #+#    #+#             */
-/*   Updated: 2016/01/20 18:12:12 by fcapocci         ###   ########.fr       */
+/*   Updated: 2016/01/18 17:41:20 by fcapocci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,69 +32,27 @@ static int		savebuffer(int const fd, char **save)
 	return (1);
 }
 
-static int		add_maill(int fd, t_files **start, t_files **files)
+int				get_next_line(int const fd, char **line)
 {
-	t_files			*new;
-
-	if ((new = (t_files*)ft_memalloc(sizeof(t_files))) == NULL)
-		return (-1);
-	if ((savebuffer(fd, &new->buff)) == -1)
-	{
-		ft_memdel((void**)&new);
-		return (-1);
-	}
-	if (new->buff == NULL)
-	{
-		ft_memdel((void**)&new);
-		return (0);
-	}
-	new->save = fd;
-	new->next = (*start);
-	(*start) = new;
-	(*files) = new;
-	return (1);
-}
-
-static char		*save_line(char **buff)
-{
-	char			*new_line;
+	static t_files	files;
 	char			*ptr;
 	char			*tmp;
 
-	if ((ptr = ft_strchr((*buff), '\n')) == NULL)
-		new_line = ft_strdup(*buff);
-	else
-		new_line = ft_strsub((*buff), 0, ptr - (*buff));
-	if ((tmp = ft_strsub((*buff), ptr - (*buff) + 1, ft_strlen(*buff))) == NULL)
-		return (NULL);
-	ft_memdel((void**)buff);
-	(*buff) = tmp;
-	return (new_line);
-}
-
-int				get_next_line(int const fd, char **line)
-{
-	static t_files	*start_files;
-	t_files			*files;
-	int				retour;
-
 	if (fd < 0 || line == NULL)
 		return (-1);
-	files = start_files;
-	while (files != NULL)
+	if (files.save != fd)
 	{
-		if (files->save == fd)
-			break ;
-		files = files->next;
+		files.save = fd;
+		ft_memdel((void**)&files.buff);
 	}
-	if (files == NULL)
-		if ((retour = add_maill(fd, &start_files, &files)) <= 0)
-			return ((retour == 0) ? 0 : -1);
-	if ((savebuffer(fd, &files->buff)) == -1)
+	if ((savebuffer(fd, &files.buff)) == -1)
 		return (-1);
-	if ((files->buff == NULL) || (ft_strlen(files->buff) == 0))
+	if ((files.buff == NULL) || (ft_strlen(files.buff) == 0))
 		return (0);
-	if ((*line = save_line(&files->buff)) == NULL)
-		return (-1);
+	ptr = ft_strchr(files.buff, '\n');
+	*line = ft_strndup(files.buff, ptr - files.buff);
+	tmp = ft_strsub(files.buff, ptr - files.buff + 1, ft_strlen(files.buff));
+	ft_memdel((void**)&files.buff);
+	files.buff = tmp;
 	return (1);
 }
--- get_next_line/safe_get_next_line.h
+++ get_next_line/safe_get_next_line.h
@@ -6,25 +6,24 @@
 /*   By: fcapocci <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/04 11:21:09 by fcapocci          #+#    #+#             */
-/*   Updated: 2016/01/20 17:45:44 by fcapocci         ###   ########.fr       */
+/*   Updated: 2016/01/18 17:42:40 by fcapocci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include "libft/libft.h"
+# include "./libft/libft.h"
 # include <unistd.h>
 # include <stdlib.h>
-# define BUFF_SIZE 42
+# define BUFF_SIZE 1
 
-int								get_next_line(int const fd, char **line);
+int				get_next_line(int const fd, char **line);
 
-typedef struct					s_files
+typedef struct	s_files
 {
-	char				*buff;
-	int					save;
-	struct s_files		*next;
-}								t_files;
+	char		*buff;
+	int			save;
+}				t_files;
 
 #endif
