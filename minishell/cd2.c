900c1587257d7204e40dadf5ff81b1528e26de64
Florent CAPOCCI <fcapocci@e1r5p14.42.fr>
2016-03-29 12:29:01 +0200

    29/03/16 12:29:01

--- minishell/cd2.c
+++ minishell/cd2.c
@@ -6,7 +6,7 @@
 /*   By: fcapocci <fcapocci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/28 21:50:07 by fcapocci          #+#    #+#             */
-/*   Updated: 2016/03/29 00:21:19 by fcapocci         ###   ########.fr       */
+/*   Updated: 2016/03/29 12:28:43 by fcapocci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,9 @@ t_env			*cd_env(t_env *vlist, char **args)
 {
 	char		mem[256];
 	char		*line;
+	char		**split;
 
+	split = NULL;
 	if (len_y(args) != 2)
 		return (vlist);
 	getcwd(mem, 256);
@@ -50,17 +52,21 @@ t_env			*cd_env(t_env *vlist, char **args)
 	if (move_ok(vlist, args[1]) != 1)
 		return (vlist);
 	line = ft_strjoin("unsetenv ", "OLDPWD");
-	unset_env(&vlist, ft_strsplit(line, ' '));
+	unset_env(&vlist, (split = ft_strsplit(line, ' ')));
+	ft_free_strsplit(&split);
 	ft_memdel((void**)&line);
 	line = ft_strjoin("setenv OLDPWD=", mem);
-	vlist = set_env(vlist, ft_strsplit(line, ' '), 0);
+	vlist = set_env(vlist, (split = ft_strsplit(line, ' ')), 0);
+	ft_free_strsplit(&split);
 	ft_memdel((void**)&line);
 	getcwd(mem, 256);
 	line = ft_strjoin("unsetenv ", "PWD");
-	unset_env(&vlist, ft_strsplit(line, ' '));
+	unset_env(&vlist, (split = ft_strsplit(line, ' ')));
+	ft_free_strsplit(&split);
 	ft_memdel((void**)&line);
 	line = ft_strjoin("setenv PWD=", mem);
-	vlist = set_env(vlist, ft_strsplit(line, ' '), 0);
+	vlist = set_env(vlist, (split = ft_strsplit(line, ' ')), 0);
+	ft_free_strsplit(&split);
 	ft_memdel((void**)&line);
 	return (vlist);
 }
