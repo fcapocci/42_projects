cb9a3cd553f5f5bf716f2e5dca09daaf27a3abf8
Capocci Florent <fcapocci@Capoccis-Mac.local>
2016-04-30 19:36:12 +0200

    30/04/16 19:36:12

--- fdf/manage_mlx.c
+++ fdf/manage_mlx.c
@@ -6,7 +6,7 @@
 /*   By: fcapocci <fcapocci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/28 21:33:42 by fcapocci          #+#    #+#             */
-/*   Updated: 2016/04/28 22:27:49 by fcapocci         ###   ########.fr       */
+/*   Updated: 2016/04/29 15:00:37 by fcapocci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,23 @@ int				manage_mlx(t_data *d_list)
 {
 	void		*mlx;
 	void		*window;
+	int			x;
+	int			y;
 
+	y = 260;
 	d_list = d_list ? d_list : NULL;
 	mlx = mlx_init();
-	window = mlx_new_window(mlx,500 , 500, "fdf");
-	sleep(5);
+	window = mlx_new_window(mlx,1500 , 800, "fdf");
+	while (y < 520)
+	{
+		x = 500;
+		while (x < 1000)
+		{
+			mlx_pixel_put(mlx, window, x, y, 0xFF0000);
+			x++;
+		}
+		y++;
+	}
+	sleep(10);
 	return (0);
 }
