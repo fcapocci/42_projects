630e434437e6795255f6767d0c09572374a881f6
Florent CAPOCCI <fcapocci@e1r4p11.42.fr>
2016-06-24 22:30:21 +0200

    J09/ex02 finished

---
+++ piscine_PHP/J09/ex02/todo.js
@@ -0,0 +1,60 @@
+var list = new Array();
+
+function load()
+{
+	var cook = getCookie("cook").split(',');
+	for (var i = 0; i < cook.length; i++)
+	{
+		add(cook[i]);
+		console.log(cook[i]);
+	}
+
+}
+
+function setCookie(cname, cvalue, exdays)
+{
+
+   var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname)
+{
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ')
+        	c = c.substring(1);
+        if (c.indexOf(name) == 0)
+        	return c.substring(name.length, c.length);
+    }
+    return "";
+}
+
+function add(todo)
+{
+	var newdiv = document.createElement("div");
+	newdiv.innerHTML = todo;
+	newdiv.style.cursor = "pointer";
+	newdiv.onclick = function()
+	{
+		if (confirm("Do you really want to delete that ? Seems important to me ? Did you even try'ed to do it ?"))
+			newdiv.parentNode.removeChild(newdiv);
+	}
+	verif = document.getElementById("ft_list");
+	document.getElementById("ft_list").insertBefore(newdiv, verif.firstChild);
+	list.push(todo);
+	setCookie('cook', list, 1);
+
+
+}
+
+function test()
+{
+	var todo = prompt("WHAT DO YOU NEED TO DO THAT YOU CAN'T REMEMBER ?");
+	if (todo != null)
+		add(todo);
+}
